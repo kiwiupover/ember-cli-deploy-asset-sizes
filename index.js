2f592cbb54775da3a9c085e45faca55518018934
Dave Laird <kiwiupover@gmail.com>
2016-07-03 21:20:05 -0700

    basic data

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+var Keen = require('keen-io');
+
 module.exports = {
-  name: 'ember-cli-deploy-assets-sizes'
+  name: 'ember-cli-deploy-assets-sizes',
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      didBuild: function(context) {
+        console.log('outputPath', context.distDir);
+        var AssetSizePrinter = require('ember-cli/lib/models/asset-size-printer');
+        var sizePrinter = new AssetSizePrinter({
+          ui: this.ui,
+          outputPath: context.config.distDir
+        });
+
+        // TODO add fall back if makeAssetSizesObject is not a function
+        return sizePrinter.makeAssetSizesObject().then(function(assets){
+          console.log('assets', assets);
+          return;
+        });
+      }
+    };
+
+    return new DeployPlugin();
+  }
+
 };
