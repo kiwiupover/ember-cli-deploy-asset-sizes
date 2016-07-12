6831512598b809ce1bf49503f0c2840a05a03089
Dave Laird <kiwiupover@gmail.com>
2016-07-12 08:36:23 -0700

    Adding asset graph

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 /* jshint node: true */
 'use strict';
 
-var Keen = require('keen-io');
+var Promise = require('ember-cli/lib/ext/promise');
+
+var sendDeployData = function(assets, config) {
+  var keen = require('keen.io');
+
+  var keen = keen.configure({
+    projectId: config.KEEN_PROJECT_ID,
+    writeKey: config.KEEN_WRITE_KEY
+  });
+
+  var pushedAsset = assets.map(function(asset){
+    asset.name = asset.name.split(/-[a-f0-9]{32}/ig).join('');
+    return asset;
+  })
+
+  return new Promise(function(resolve, reject) {
+
+    let keenPayload = JSON.stringify({ deploy: pushedAsset });
+
+    keen.addEvents(keenPayload, function(err, res) {
+      console.log('keen-data', err, res);
+      if(err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
 
 module.exports = {
   name: 'ember-cli-deploy-assets-sizes',
@@ -10,23 +38,22 @@ module.exports = {
     return {
       name: options.name,
 
-      didBuild: function(context) {
-        console.log('outputPath', context.distDir);
+      willUpload: function(context) {
         var AssetSizePrinter = require('ember-cli/lib/models/asset-size-printer');
         var sizePrinter = new AssetSizePrinter({
           ui: this.ui,
-          outputPath: context.config.distDir
+          outputPath: context.project.root + '/' + context.distDir
         });
+
+        var emberCliDeployAssetsSizesConfig = context.config.emberCliDeployAssetsSizes;
 
         // TODO add fall back if makeAssetSizesObject is not a function
         return sizePrinter.makeAssetSizesObject().then(function(assets){
-          console.log('assets', assets);
-          return;
+          return sendDeployData(assets, emberCliDeployAssetsSizesConfig);
         });
       }
     };
 
     return new DeployPlugin();
   }
-
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    KEEN_PROJECT_ID: '5702ff5ae0855707f784b1e1',
+    KEEN_WRITE_KEY: '7cf8cb0f9a5d38bae16503fc490a17ea399242cbe40e2097c3613784ddb0d0811a9977cbeff5b3e8b15d5a57fe20cb5a7bd706afd45f4ac8213c5c5b7502f778f8af42943c8cfb2533994f5d638f112ff44f21308d7ade487f61f97c1d3af54a',
+    KEEN_READ_KEY: 'b4884962ba2f6022ef84df329196509807936e4439094919088c7c29abac1771e91c62dd5e3c51f0a45fe41e87997f9358ffe5bced060430b7ebbd47f6e228b4d4c4b3737a8679724190fa39d5d6c960ed2636578c3367977978abc670bb679c',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
